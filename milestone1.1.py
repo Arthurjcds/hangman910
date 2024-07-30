ef2c3b6acd92ef7d176d2933d0413d8bcc6b54ec
Arthur <arthurjcds@gmail.com>
2024-07-30 12:04:23 +0200

    This scripts picks a random fruit from a list

---
+++ milestone1.1.py
@@ -0,0 +1,9 @@
+print("This picks a random word from a list")
+
+import random
+word_list = ["apple", "mango", "pineapple", "grapefruit", "strawberry"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
